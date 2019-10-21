f2091a569d5945e4e40c8fbec5a4cf58e1617bd5
Kentleigh English <kentleigh.english@bravissimo.com>
2019-10-21 15:38:47 +0100

    Update to dynamic email list

--- index.js
+++ index.js
@@ -2,67 +2,81 @@ const express = require("express");
 const nodemailer = require('nodemailer');
 const app = express();
 const config = require('config');
+const fs = require('fs');
 
 app.use(express.static("public"));
 app.use(express.json());
 
 app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/views/index.html");
+    try {
+        fs.readFile(__dirname + "/views/index.html", 'utf8', (err, data) => {
+            if (err) {
+                throw err;
+            }
+            
+            const output = data.replace('%EMAIL_LIST%', config.emailList);
+            
+            response.send(output);
+        });
+    } catch(e) {
+        console.error(e);
+        repsonse.send('An unexpected error occurred');
+    }
 });
 
 app.post('/submit', async (request, response) => {
-  const data = request.body;
-  
-  if (data.email && data.body && data.name) {
-    let transporter = nodemailer.createTransport({
-        host: 'mail.ikengainnovations.com',
-        port: 587,
-        secure: false,
-        auth: {
-            user: config.smtp.username,
-            pass: config.smtp.password
-        }
-    });
-    
-    const body = `${data.body.replace(/\n/g, "<br>")}<br>${data.name}`;
-    
-    const from = `"${data.name}" <${data.email}>`;
-  
-    console.log("Form submitted from", from);
-    try {
-      const info = await transporter.sendMail({
-          from,
-          to: config.emailList,
-          cc: config.ccList.split(','),
-          subject: `Call the VONC - ${data.name}`,
-          text: body,
-          html: body
-      });
-      
-      console.log(info);
-      
-      response.json({
-        success: true
-      });
-    } catch(e) {
-      console.error(e);
-      
-      response.json({
-        success: false
-      });
-    }
-  } else {
-    response.json({
-      success: false
-    });
-  }
+	const data = request.body;
+
+	if (data.email && data.body && data.name) {
+		let transporter = nodemailer.createTransport({
+			host: 'mail.ikengainnovations.com',
+			port: 587,
+			secure: false,
+			auth: {
+				user: config.smtp.username,
+				pass: config.smtp.password
+			}
+		});
+
+		const body = `${data.body.replace(/\n/g, "<br>")}<br>${data.name}`;
+
+		const from = `"${data.name}" <${data.email}>`;
+
+		console.log("Form submitted from", from);
+		try {
+			const info = await transporter.sendMail({
+				from,
+				to: config.emailList,
+				cc: config.ccList.split(','),
+				subject: `Call the VONC - ${data.name}`,
+				text: body,
+				html: body
+			});
+
+			console.log(info);
+
+			response.json({
+				success: true
+			});
+		} catch (e) {
+			console.error(e);
+
+			response.json({
+				success: false
+			});
+		}
+	} else {
+		response.json({
+			success: false
+		});
+	}
 });
 
 if (config.emailList && config.ccList) {
-    // listen for requests :)
-    const listener = app.listen(config.port, function() {
-        console.log("Your app is listening on port " + listener.address().port);
-    });
+	// listen for requests :)
+	const listener = app.listen(config.port, function() {
+		console.log("Your app is listening on port " + listener.address().port);
+	});
 } else {
-    throw 'You need to set EMAIL_LIST and CC_LIST environment variables';
+	throw 'You need to set EMAIL_LIST and CC_LIST environment variables';
 }
