5607c0eee80e8fc5da4ecdf0ba22a5837def48b9
Kentleigh English <kentleigh@gmx.com>
2019-10-22 03:25:01 +0100

    Add angular js and socket io

--- index.js
+++ index.js
@@ -8,20 +8,83 @@ app.use(express.static("public"));
 app.use(express.json());
 
 app.get("/", function(request, response) {
-    try {
-        fs.readFile(__dirname + "/views/index.html", 'utf8', (err, data) => {
-            if (err) {
-                throw err;
-            }
-            
-            const output = data.replace('%EMAIL_LIST%', config.emailList.replace(/,/g, ', '));
-            
-            response.send(output);
-        });
-    } catch(e) {
-        console.error(e);
-        repsonse.send('An unexpected error occurred');
-    }
+	const state = {
+		"title": "Email The NEC",
+		"emailCount": 0,
+		"content": "We seek to demand that the NEC",
+		"form": {
+			"type": "inline",
+			"fields": [
+				{ "name": "branch_name", "type": "text" },
+				{ "name": "meeting_location", "type": "text" },
+				{ "name": "meeting_date", "type": "text" },
+				{ "name": "proposer_name", "type": "text" },
+				{ "name": "proposer_role", "type": "text" },
+				{ "name": "seconder_name", "type": "text" },
+				{ "name": "seconder_role", "type": "text" },
+				{ "name": "members_present", "type": "number" },
+				{ "name": "proxy_votes", "type": "number" },
+				{ "name": "abstention_votes", "type": "number" },
+				{ "name": "favour_votes", "type": "number" },
+				{ "name": "against_votes", "type": "number" },
+				{ "name": "signature_name", "type": "text" },
+				{ "name": "signature_role", "type": "text" },
+				{ "name": "signature_branch", "type": "text" },
+				{ "name": "signature_phone", "type": "text" },
+			],
+			"body": "To Kirstan Herriot – UKIP Party Chair\
+\
+Subject: Motion of “No Confidence” in the NEC.\
+\
+Without prejudice to any members participating the following motion was proposed at the {{branch_name}} Monthly Branch Meeting, held at the {{meeting_location}} on {{meeting_date}}.\
+\
+Preamble:\
+The members present believe and agreed that the NEC in both refusing to allow Gerard Batten to stand for election in the Leadership Election, and further by not allowing the new leader Richard Braine, duly elected on 10th August to appoint his choice of both Deputy Leader and Chairperson which he had publicly and openly said and campaigned he would do in the run up to and at hustings held before the election is a blatant blight on democratic process and is not coherent with “Natural Justice” as mentioned throughout the constitution in both ‘nemo iudex in causa sua’ and ‘audi alteram partem’ .\
+\
+The Motion read thus:\
+That this, {{branch_name}} Branch, of UKIP do hereby call, after a members vote of “NO CONFIDENCE” in the NEC that the entire body of the NEC stand down with immediate effect.\
+\
+\
+Proposed by:	{{proposer_name}}					Signature:\
+		{{proposer_role}}.\
+\
+Seconded by:	{{seconder_name}}			Signature:\
+		{{seconder_role}}\
+\
+THE VOTE:	{{members_present}} (seventeen) members present\
+		{{proxy_votes}} Proxy votes.\
+\
+Abstention 	{{abstention_votes}}\
+In Favour	{{favour_votes}}\
+Against		{{against_votes}}\
+\
+Result:		{{favour_votes}} - FOR.	{{against_votes}} - AGAINST\
+\
+\
+\
+{{signature_name}},\
+{{signature_role}}\
+{{signature_branch}}\
+Telephone: 	{{signature_phone}}"
+		}
+	};
+
+	try {
+		fs.readFile(__dirname + "/views/index.html", 'utf8', (err, data) => {
+			if (err) {
+				throw err;
+			}
+
+			const output = data
+				.replace('%%STATE%%', JSON.stringify(state))
+				.replace('%%TITLE%%', state.title);
+
+			response.send(output);
+		});
+	} catch (e) {
+		console.error(e);
+		repsonse.send('An unexpected error occurred');
+	}
 });
 
 app.post('/submit', async (request, response) => {
@@ -29,9 +92,9 @@ app.post('/submit', async (request, response) => {
 
 	if (data.email && data.body && data.name) {
 		let transporter = nodemailer.createTransport({
-			host: 'mail.ikengainnovations.com',
-			port: 587,
-			secure: false,
+			host: config.smtp.host,
+			port: config.smtp.port,
+			secure: ! !+config.smtp.secure,
 			auth: {
 				user: config.smtp.username,
 				pass: config.smtp.password
@@ -55,20 +118,14 @@ app.post('/submit', async (request, response) => {
 
 			console.log(info);
 
-			response.json({
-				success: true
-			});
+			response.json({success: true});
 		} catch (e) {
 			console.error(e);
 
-			response.json({
-				success: false
-			});
+			response.json({success: false});
 		}
 	} else {
-		response.json({
-			success: false
-		});
+		response.json({success: false});
 	}
 });
 
--- public/client.js
+++ public/client.js
@@ -1,56 +1,138 @@
 // client-side js
 
-const form = document.forms[0];
-const inputs = form.querySelectorAll('input,textarea');
-const button = document.getElementById('submitForm');
-const successMessage = document.getElementById('successMessage');
+const app = angular.module('app', []);
 
-const data = {
-};
+app.service('apiService', function() {
+	return {
+		submitForm: function() {
+			return new Promise(function(resolve, reject) {
+				const xhttp = new XMLHttpRequest();
+				xhttp.onreadystatechange = function() {
+					if (this.readyState === 4) {
+						if (this.status == 200) {
+							const response = JSON.parse(xhttp.responseText);
 
-function updateFormData(key, value) {
-  data[key] = value;
-  
-  const input = document.querySelector('input[name="'+key+'"],textarea[name="'+key+'"]');
-  
-  input.value = data[key];
-}
-
-function sendForm() {
-  const xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState === 4) {
-      if (this.status == 200) {
-        const response = JSON.parse(xhttp.responseText);
-
-        if (response.success) {
-          button.setAttribute('disabled', false);
-          successMessage.style.display = 'block';
-          form.style.display = 'none';
-        } else {
-          button.setAttribute('disabled', false);
-        }
-      }
-    }
-  };
-  xhttp.open("POST", '/submit', true);
-  xhttp.setRequestHeader('Content-Type', 'application/json');
-  xhttp.send(JSON.stringify(data));
-}
-
-// iterate through every dream and add it to our page
-inputs.forEach(function(input) {
-  updateFormData(input.name, input.value);
-  input.addEventListener('input', function(event) {
-    updateFormData(input.name, event.target.value);
-  });
+							if (response.success) {
+								resolve();
+							} else {
+								reject();
+							}
+						}
+					}
+				};
+				xhttp.open("POST", '/submit', true);
+				xhttp.setRequestHeader('Content-Type', 'application/json');
+				xhttp.send(JSON.stringify(data));
+			})
+		}
+	}
 });
 
-// listen for the form to be submitted and add a new dream when it is
-form.onsubmit = function(event) {
-  // stop our form submission from refreshing the page
-  event.preventDefault();
-  button.setAttribute('disabled', true);
-  
-  sendForm();
-};
+app.controller('AppController', ['$scope', 'apiService', function($scope, apiService) {
+	this.state = window.__INITIAL_STATE__;
+
+	this.form = Object(this.state.form);
+
+	this.formData = {};
+
+	this.parse = function(content) {
+		return content;
+		return $interpolate(content)(this.formData);
+	}
+
+	this.updateForm = function(form) {
+		console.log(form);
+	}
+}]);
+
+app.directive('inlineForm', function() {
+	return {
+		restrict: 'A',
+		scope: {
+			inlineForm: '<',
+			form: '<',
+			update: '&'
+		},
+		controllerAs: 'vm',
+		controller: [ '$scope', '$compile', '$interpolate', function($scope, $compile, $interpolate) {
+			this.$compile = $compile;
+			this.$interpolate = $interpolate;
+		}],
+		link: function($scope, $element, attrs, ctrl) {
+			const input = $scope.inlineForm;
+
+			ctrl.onUpdate = function() {
+				$scope.update({ form: ctrl.form })
+			}
+
+			output = input.body.replace(/\n/g, '<br>');
+
+			const fields = input.fields.reduce(function(obj, input) {
+				obj[input.name] = '<input ng-change="vm.onUpdate()" ng-model="vm.form[\''+input.name+'\']" name="'+input.name+'" type="'+input.type+'" required="'+!!input.required+'" />';
+
+				return obj;
+			}, {});
+
+			output = ctrl.$interpolate(output)(fields);
+
+			output = ctrl.$compile('<div>'+output+'</div>')($scope);
+
+			$element.replaceWith(output);
+		}
+	}
+});
+
+// const form = document.forms[0];
+// const inputs = form.querySelectorAll('input,textarea');
+// const button = document.getElementById('submitForm');
+// const successMessage = document.getElementById('successMessage');
+//
+// const data = {
+// };
+//
+// function updateFormData(key, value) {
+//   data[key] = value;
+//
+//   const input = document.querySelector('input[name="'+key+'"],textarea[name="'+key+'"]');
+//
+//   input.value = data[key];
+// }
+//
+// function sendForm() {
+//   const xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function() {
+//     if (this.readyState === 4) {
+//       if (this.status == 200) {
+//         const response = JSON.parse(xhttp.responseText);
+//
+//         if (response.success) {
+//           button.setAttribute('disabled', false);
+//           successMessage.style.display = 'block';
+//           form.style.display = 'none';
+//         } else {
+//           button.setAttribute('disabled', false);
+//         }
+//       }
+//     }
+//   };
+//   xhttp.open("POST", '/submit', true);
+//   xhttp.setRequestHeader('Content-Type', 'application/json');
+//   xhttp.send(JSON.stringify(data));
+// }
+//
+// // iterate through every dream and add it to our page
+// inputs.forEach(function(input) {
+//   updateFormData(input.name, input.value);
+//   input.addEventListener('input', function(event) {
+//     updateFormData(input.name, event.target.value);
+//   });
+// });
+//
+// // listen for the form to be submitted and add a new dream when it is
+// form.onsubmit = function(event) {
+//   // stop our form submission from refreshing the page
+//   event.preventDefault();
+//   button.setAttribute('disabled', true);
+//
+//   sendForm();
+// };
